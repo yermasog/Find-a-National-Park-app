4b777ce91bd6cd52cc521326e7c7101d0460f732
Gustaf Gonzalez <tony.campoy@gmail.com>
2021-01-14 12:54:46 -0600

    update

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,11 +1,17 @@
-var searchterm = "yellowstone";
-var queryURL = "https://developer.nps.gov/api/v1/parks?parkCode=" + searchterm 
+var searchterm = $(this).val();
+var queryURL = "https://developer.nps.gov/api/v1/activities/parks?q" + searchterm 
 + "&api_key=ntIG3OA71FDbXqFK26t4ABXRfYhcgtL8l5nJ9z8N";
 
-$("testbtn").on("click", function(){
+$("#boating").toggle(function(){
+    alert(works);
     $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response){
         console.log(response);})
 })
+
+$('#switch-toggle-all [data-toggle-all]' ).click(function () {
+    $( '#switch-toggle-all input[type="checkbox"]').prop('checked', this.checked)
+  })
+  
