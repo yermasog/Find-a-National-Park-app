f2034c77ff47d66059e88a8e16ae97736fcdb5c3
Gustaf Gonzalez <tony.campoy@gmail.com>
2021-01-15 13:07:49 -0600

    fixed api

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -12,15 +12,45 @@
 
 
 
-var searchterm = "MN";
 
-var queryURL = "https://developer.nps.gov/api/v1/parks?stateCode=" + searchterm 
+
+$("#test-button").on("click", function(event) {
+  event.preventDefault();
+  var searchterm = ["MN", "WI", "SD", "ND"] ;
+  var queryURL = "https://developer.nps.gov/api/v1/parks?stateCode=" + searchterm 
 + "&api_key=ntIG3OA71FDbXqFK26t4ABXRfYhcgtL8l5nJ9z8N";
 
-$("#test-button").on("click", function(){
-  $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response){
-      console.log(response);})
+  var settings = {
+    async: true,
+    crossDomain: true,
+    url: queryURL,
+    method: "GET",
+    success: function(response){
+      console.log(response);
+    }
+
+  }
+
+  $.ajax(settings).then(function(response){
+      console.log(response);
+
+      var park1 = $("#park1");
+      park1.text(response.data[3].fullName);
+      $("#park1").append(park1)
+      console.log("LOOK!!!");
+      console.log(response.data);
+
+
+
+
+})
+
+// true|false for state switches
+// if/else on click event checking for toggles
+// if true return state results from array (data[0]) for loop
+// MN array 3 9 13 16
+// WI array 0 4 14 
+//  ND array 2 6 15
+// SD array 1 5 8 11 17
+
 })
