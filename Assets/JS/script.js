ce56f30828d785884000f67c8a169e57d1b4eb0f
Gustaf Gonzalez <tony.campoy@gmail.com>
2021-01-15 11:17:08 -0600

    query works

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,17 +1,16 @@
-var searchterm = "boating";
-var queryURL = "https://developer.nps.gov/api/v1/activities/parks?q" + searchterm 
+var searchterm = "MN";
+var queryURL = "https://developer.nps.gov/api/v1/activities/parks?stateCode=" + searchterm 
 + "&api_key=ntIG3OA71FDbXqFK26t4ABXRfYhcgtL8l5nJ9z8N";
 
-$("#submit").toggle(function(){
-    alert(works);
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response){
-        console.log(response);})
+$("#submit").on("click", function(){
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function(response){
+    console.log(response);
+  })
 })
+    
 
-// $('#switch-toggle-all [data-toggle-all]' ).click(function () {
-//     $( '#switch-toggle-all input[type="checkbox"]').prop('checked', this.checked)
-//   })
-  
+
+
