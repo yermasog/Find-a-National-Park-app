a80cb6d82967df4fbcadb26f0e38d4de83a22279
ymasog <yer.masog@gmail.com>
2021-01-16 12:43:42 -0600

    added loop to append data to page

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -11,24 +11,26 @@
 // })
 
 
-// Minnesota
 
-$(".button").on("click", function (event) {
+$(".submit-button").on("click", function (event) {
   event.preventDefault();
   var searchterm = $(this).attr("id")
   var queryURL = "https://developer.nps.gov/api/v1/parks?stateCode=" + searchterm
     + "&api_key=ntIG3OA71FDbXqFK26t4ABXRfYhcgtL8l5nJ9z8N";
 
-  var settings = {
-    async: true,
-    crossDomain: true,
+  // var settings = {
+  //   async: true,
+  //   crossDomain: true,
+  //   url: queryURL,
+  //   method: "GET",
+  //   success: function (response) {
+  //     console.log(response);
+  //   }
+  // }
+  $.ajax({
     url: queryURL,
-    method: "GET",
-    success: function (response) {
-      // console.log(response);
-    }
-  }
-  $.ajax(settings).then(function (response) {
+    method: "GET"
+  }).then(function (response) {
     console.log(response);
 
     for (let i = 0; i < (response.total); i++) {
@@ -65,108 +67,42 @@ $(".button").on("click", function (event) {
       bigDiv.append(photoDiv);
       postText.append(postSummaryDiv);
       bigDiv.append(postText);
-
+      //append everything to the main div
       section.append(bigDiv);
+
+      // use the lat and lon from NPS API to feed into open weather API to get weather info
+      // var lat = respponse.data[0].latitude;
+      // var lon = response.data[0].longitude;
+
+      // var weatherURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=18687cf1c7c1d78e93e8472d225dee33&units=imperial"
+      // $.ajax({
+      //   url: weatherURL,
+      //   method: "GET"
+      // }).then(function (weather) {
+      //   console.log(weather)
+
+      //   var temp = weather.current.temp 
+      //   var icon = weather.weather[0].id
+      //   var dailyicon = "https://openweathermap.org/img/w/" + icon + ".png"
+      //   var weatherDes = weather.weather[0].description
+
+
+      // })
+
     }
-  })
-
-})
-
-// Wisconsin
-
-$("#wiSwitch").on("click", function (event) {
-  event.preventDefault();
-  var searchterm = ["WI"];
-  var queryURL = "https://developer.nps.gov/api/v1/parks?stateCode=" + searchterm
-    + "&api_key=ntIG3OA71FDbXqFK26t4ABXRfYhcgtL8l5nJ9z8N";
-
-  var settings = {
-    async: true,
-    crossDomain: true,
-    url: queryURL,
-    method: "GET",
-    success: function (response) {
-      console.log(response);
-    }
-  }
-  $.ajax(settings).then(function (response) {
-    console.log(response);
-
-    var park1 = $("#park1");
-    park1.text(response.data[3].fullName);
-    $("#park1").append(park1)
-    console.log("LOOK!!!");
-    console.log(response.data);
-
-  })
-
-})
-
-// North Dakota
-
-$("#ndSwitch").on("click", function (event) {
-  event.preventDefault();
-  var searchterm = ["ND"];
-  var queryURL = "https://developer.nps.gov/api/v1/parks?stateCode=" + searchterm
-    + "&api_key=ntIG3OA71FDbXqFK26t4ABXRfYhcgtL8l5nJ9z8N";
-
-  var settings = {
-    async: true,
-    crossDomain: true,
-    url: queryURL,
-    method: "GET",
-    success: function (response) {
-      console.log(response);
-    }
-  }
-  $.ajax(settings).then(function (response) {
-    console.log(response);
-
-    var park1 = $("#park1");
-    park1.text(response.data[3].fullName);
-    $("#park1").append(park1)
-    console.log("LOOK!!!");
-    console.log(response.data);
-
-  })
-
-})
-
-// South Dakota
-
-$("#sdSwitch").on("click", function (event) {
-  event.preventDefault();
-  var searchterm = ["SD"];
-  var queryURL = "https://developer.nps.gov/api/v1/parks?stateCode=" + searchterm
-    + "&api_key=ntIG3OA71FDbXqFK26t4ABXRfYhcgtL8l5nJ9z8N";
-
-  var settings = {
-    async: true,
-    crossDomain: true,
-    url: queryURL,
-    method: "GET",
-    success: function (response) {
-      console.log(response);
-    }
-  }
-  $.ajax(settings).then(function (response) {
-    console.log(response);
-
-    var park1 = $("#park1");
-    park1.text(response.data[3].fullName);
-    $("#park1").append(park1)
-    console.log("LOOK!!!");
-    console.log(response.data);
 
   })
 
 })
 
 
-// true|false for state switches
-// if/else on click event checking for toggles
-// if true return state results from array (data[0]) for loop
-// MN array 3 9 13 16
-// WI array 0 4 14 
-//  ND array 2 6 15
-// SD array 1 5 8 11 17
+
+
+
+// // true|false for state switches
+// // if/else on click event checking for toggles
+// // if true return state results from array (data[0]) for loop
+// // MN array 3 9 13 16
+// // WI array 0 4 14 
+// //  ND array 2 6 15
+// // SD array 1 5 8 11 17
