eb10b48cef178399a89e4a8f07663d1a0df9e83d
Gustaf Gonzalez <tony.campoy@gmail.com>
2021-01-19 12:31:10 -0600

    began work on all button

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,19 +1,13 @@
-// var searchterm = "yellowstone";
-// var queryURL = "https://developer.nps.gov/api/v1/parks?parkCode=" + searchterm 
-// + "&api_key=ntIG3OA71FDbXqFK26t4ABXRfYhcgtL8l5nJ9z8N";
+$("#allBtn").on("click", function(){
+  
+})
 
-// $("testbtn").on("click", function(){
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//       }).then(function(response){
-//         console.log(response);})
-// })
+
+
 
 
 
 $(".submit-button").on("click", function (event) {
-  // alert("testing")
   event.preventDefault();
   $(this).attr("disabled", true);
   $(this).val("submitted");
